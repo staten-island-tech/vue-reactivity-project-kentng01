21684b95dd85870cada725202760e9ee53b26b6e
kentng01 <57498837+kentng01@users.noreply.github.com>
2021-10-28 12:29:53 -0400

    ehehehe

--- rent-a-waifu/src/App.vue
+++ rent-a-waifu/src/App.vue
@@ -3,21 +3,45 @@
   <div class="app">
 
   <div class="btn-container">
-    <div class="updatebutton" v-for="product in Products" :key="product.productName">
-          <button id="btn-selections" @click="updateProduct(product.image, product.productName, product.description)">{{product.productName}}</button> 
+    <div class="updatebutton" 
+    v-for="product in Products" 
+    :key="product.productName">
+          <button id="btn-selections"
+           @click="updateProduct(product.image, product.productName, product.description)">{{product.productName}}</button> 
     </div>
   </div>
 
-    <div class="product">
+  <div class="waifu-container">
+    <div class="drifting-luminescence">
+      <div class="waifu-img">
+        <img 
+        :src="image" 
+        alt="" 
+        class="img-waifu">
+        <div class="buttons">
+          <button id="btn-waifu" v-on:click="addToCart">Add to Cart</button>
+          <div class="cart-container">
+            <p class="cart">Cart({{cart}})</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
+  </div>
+
+    <!-- <div class="product">
+      <div class="container">
       <div class="product-info">
         <h2 class="product-name">{{productName + " " +description}}</h2>
-      </div>
 
-      <img :src="image" alt="" class="image">
+      <img :src="image" alt="" class="img-waifu">
 
       <div class="story">Storyline
         <h1 v-for="story in story" :key="story">{{story}}</h1>
+      </div>
+
+      </div>
       </div>
 
       <div class="buttons">
@@ -30,7 +54,8 @@
       </div>  
 
         </div>
-      </div>  
+      </div>   -->
+
     
 </template>
 
@@ -93,18 +118,27 @@ export default {
 
 <style>
 
-  html,
-  body {
-    font-family: "Source Sans Pro", sans-serif;
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-    font-size: 92.5%;
-    color: black;
-    height: 100vh;
-    font-weight: 500;
-  }
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap');
 
+* {
+  padding: 0;
+  margin: 0;
+  font-size: 95%;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+}
+
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  font-family: 'Noto Sans JP', sans-serif;
+}
 
 /* app */
 
@@ -117,10 +151,22 @@ export default {
     margin-top: 60px;
   }
   
-  .product{
-    margin: 4rem;
-    margin-top: 0;
-  }
+/* container */
+
+.waifu-container {
+    background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(./assets/backgrounds/christmas.jpg) no-repeat top ;
+    margin: 0;
+    width: 80rem;
+    height: 45rem;
+    max-width: 100rem;
+    min-width: 50rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    border-radius: 20px;
+}
 
 /* button styles */
 
@@ -135,6 +181,7 @@ export default {
     text-decoration: none;
     text-align: center;
     z-index: 2;
+    margin-bottom: 1.5rem;
   }
 
   .buttons {
@@ -154,13 +201,13 @@ export default {
   }
 
   #btn-selections {
-    width: 25rem;
-    height: 5rem;
+    width: 12rem;
+    height: 6rem;
     border: none;
     font-family: "Noto Sans JP", serif;
     text-transform: uppercase;
     cursor: pointer;
-    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5));
+    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5)), url(./assets/backgrounds/christmas.jpg) no-repeat bottom;
     color: #ffffff;
     margin-top: 1.5rem;
     padding: 0.5rem 1.5rem;
@@ -171,10 +218,11 @@ export default {
     box-shadow: 0 0.3rem 0.5rem rgba(0, 0, 0, 0.4);
     transition: all 0.7s;
     border-radius: 15px;
+    line-height: 1;
   }
 
   #btn-selections:hover {
-    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7));
+    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7)), url(./assets/backgrounds/halloween.jpg) no-repeat center;
     color: rgb(11, 3, 41);
     transform: scale(1.05);
   }
@@ -190,7 +238,7 @@ export default {
     font-family: "Noto Sans JP", serif;
     text-transform: uppercase;
     cursor: pointer;
-    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5));
+    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5)), url(./assets/backgrounds/christmas.jpg) no-repeat center ;
     color: #ffffff;
     margin-top: 1.5rem;
     padding: 0.5rem 1.5rem;
@@ -204,7 +252,7 @@ export default {
   }
 
   #btn-waifu:hover {
-    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7));
+    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7)), url(./assets/backgrounds/halloween.jpg) no-repeat center ;
     color: rgb(11, 3, 41);
     transform: scale(1.05);
   }
@@ -215,11 +263,9 @@ export default {
 
 /* images */
 
-
-  .image{
+  .img-waifu{
     width: 25%;
     height: 100%;
-
   }
 
   .cart{
@@ -237,5 +283,52 @@ export default {
     display: flex;
     flex-wrap: wrap;
   }
+
+/* test */
+
+
+
+/* container */
+
+.waifu-container {
+    background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(/images/5cm.jpg) no-repeat center ;
+    margin: 0;
+    width: 60rem;
+    max-width: 80rem;
+    min-width: 40rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    border-radius: 20px;
+}
+
+
+/* content */
+
+.waifu-selections {
+  display: flex;
+  flex-direction: column;
+}
+
+/* img settings */
+
+.waifu-img {
+  align-self: center;
+  margin-top: 4rem;
+}
+
+#kawaii-waifu {
+  width: 100%;
+  height: auto;
+  border-radius: 20px;
+}
+
+#waifu {
+  width: 75%;
+  height: auto;
+  border-radius: 20px;
+}
 
 </style>
