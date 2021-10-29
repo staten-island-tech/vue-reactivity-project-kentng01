a594ba3cb71f5602c1ef98bff710f2357f39dd4c
kentng01 <57498837+kentng01@users.noreply.github.com>
2021-10-29 12:29:16 -0400

    dasgdagdsgdas

--- rent-a-waifu/src/App.vue
+++ rent-a-waifu/src/App.vue
@@ -7,7 +7,7 @@
     v-for="product in Products" 
     :key="product.productName">
           <button id="btn-selections"
-           @click="updateProduct(product.image, product.productName, product.description)">{{product.productName}}</button> 
+           @click="updateProduct(product.image, product.productName, product.description, product.price)">{{product.productName}}</button> 
     </div>
   </div>
 
@@ -22,6 +22,7 @@
           <button id="btn-waifu" v-on:click="addToCart">Add to Cart</button>
           <div class="cart-container">
             <p class="cart">Cart({{cart}})</p>
+            <p class="cartTotal">Total:${{total}}</p>
           </div>
         </div>
       </div>
@@ -29,33 +30,6 @@
   </div>
 
   </div>
-
-    <!-- <div class="product">
-      <div class="container">
-      <div class="product-info">
-        <h2 class="product-name">{{productName + " " +description}}</h2>
-
-      <img :src="image" alt="" class="img-waifu">
-
-      <div class="story">Storyline
-        <h1 v-for="story in story" :key="story">{{story}}</h1>
-      </div>
-
-      </div>
-      </div>
-
-      <div class="buttons">
-        <button id="btn-waifu" v-on:click="addToCart">Add to Cart</button>
-
-      <div class="cart-container">
-        <p class="cart">Cart({{cart}})</p>
-        </div>
-
-      </div>  
-
-        </div>
-      </div>   -->
-
     
 </template>
 
@@ -67,6 +41,8 @@ export default {
 
     data(){
       return{
+        total: 0,
+        price: 1,
         productName:"Aether",
         description: "",
         image: require("./assets/aether.png"),
@@ -74,26 +50,30 @@ export default {
         Products:[{
 
           productName:"Ayaka",
+              price: 300,
               description: "",
               image: require("./assets/inazuma/ayaka.png"),
-              story:["no"],
               },{
 
           productName:"Raiden Ei",
+              price: 300,
               description: "",
               image: require("./assets/inazuma/ei.png"),
               },{
 
           productName:"Kujou Sara",
+              price: 300,
               description: "",
               image: require("./assets/inazuma/kujou.png"),
               },{
 
           productName:"Kokomi",
+              price: 300,
               description: "",
               image: require("./assets/inazuma/kokomi.png"),
 
               },
+              
             ],
 
           cart:0
@@ -106,11 +86,15 @@ export default {
     addToCart() {
       this.cart += 1
     },
-    updateProduct(image,productName,description ) {
+    updateProduct(image, productName, description, price ) {
       this.image = image
       this.productName = productName
       this.description = description
+      this.price = price
     },
+    updateTotal(){
+      this.total += this.price
+    }
   }
 }
 
@@ -127,6 +111,10 @@ export default {
   box-sizing: border-box;
   text-decoration: none;
   list-style: none;
+}
+
+html {
+  background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(./assets/backgrounds/cherryblossoms.jpg) no-repeat top ;
 }
 
 body {
@@ -157,7 +145,7 @@ body {
     background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(./assets/backgrounds/christmas.jpg) no-repeat top ;
     margin: 0;
     width: 80rem;
-    height: 45rem;
+    height: 77vh;
     max-width: 100rem;
     min-width: 50rem;
     display: flex;
@@ -202,7 +190,7 @@ body {
 
   #btn-selections {
     width: 12rem;
-    height: 6rem;
+    height: 5rem;
     border: none;
     font-family: "Noto Sans JP", serif;
     text-transform: uppercase;
@@ -212,7 +200,7 @@ body {
     margin-top: 1.5rem;
     padding: 0.5rem 1.5rem;
     outline: none;
-    font-size: 2rem;
+    font-size: 1.5rem;
     font-weight: bold;
     text-shadow: 0 0.2rem 0.4rem rgba(0, 0, 0, 0.2);
     box-shadow: 0 0.3rem 0.5rem rgba(0, 0, 0, 0.4);
@@ -232,28 +220,30 @@ body {
   }
 
   #btn-waifu {
-    width: 25rem;
+    width: 17.5rem;
     height: 5rem;
     border: none;
     font-family: "Noto Sans JP", serif;
     text-transform: uppercase;
     cursor: pointer;
-    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5)), url(./assets/backgrounds/christmas.jpg) no-repeat center ;
-    color: #ffffff;
+    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7)), url(./assets/backgrounds/halloween.jpg) no-repeat center ;
+    color: rgb(11, 3, 41);
     margin-top: 1.5rem;
     padding: 0.5rem 1.5rem;
     outline: none;
-    font-size: 2rem;
+    font-size: 1.75rem;
     font-weight: bold;
     text-shadow: 0 0.2rem 0.4rem rgba(0, 0, 0, 0.2);
     box-shadow: 0 0.3rem 0.5rem rgba(0, 0, 0, 0.4);
     transition: all 0.7s;
     border-radius: 15px;
+    margin-left: -27.5rem;
+    position: absolute;
   }
 
   #btn-waifu:hover {
-    background: linear-gradient(rgba(251, 249, 255, 0.7), rgba(251, 249, 255, 0.7)), url(./assets/backgrounds/halloween.jpg) no-repeat center ;
-    color: rgb(11, 3, 41);
+    background: linear-gradient(rgba(20, 4, 66, 0.5), rgba(20, 4, 66, 0.5)), url(./assets/backgrounds/christmas.jpg) no-repeat bottom ;
+    color: #ffffff;
     transform: scale(1.05);
   }
 
@@ -264,8 +254,10 @@ body {
 /* images */
 
   .img-waifu{
-    width: 25%;
+    width: 45%;
     height: 100%;
+    margin-top: -8rem;
+    margin-bottom: 4rem;
   }
 
   .cart{
@@ -275,8 +267,24 @@ body {
     width: 10rem;
     height: 3.5rem;
     text-align: center;
-    padding-top: 1rem;
+    position: absolute;
+    margin-left: 12rem;
+    margin-top: -1rem; 
+    padding: .2rem;
   }
+
+  .cartTotal {
+    font-size: 2rem;
+    background-color: #A1BFD9;
+    border-radius: 0.5rem;
+    width: 10rem;
+    height: 3.5rem;
+    text-align: center;
+    position: absolute;
+    margin-top: -1rem;
+    padding: .2rem;
+}
+
 
   .updatebutton{
     width: fit-content;
@@ -284,14 +292,10 @@ body {
     flex-wrap: wrap;
   }
 
-/* test */
-
-
-
 /* container */
 
 .waifu-container {
-    background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(/images/5cm.jpg) no-repeat center ;
+    background: linear-gradient(rgba(20, 4, 66, 0.4), rgba(20, 4, 66, 0.4)), url(./assets/backgrounds/inazuma.jpg) no-repeat center ;
     margin: 0;
     width: 60rem;
     max-width: 80rem;
